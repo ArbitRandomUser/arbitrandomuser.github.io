fb0915e28e606dd88db705594ef7ed97bd0f0740
John George Francis <arbitrandomuser@gmail.com>
2022-03-01 01:35:35 +0530

    commit Tue Mar  1 01:35:35 AM IST 2022

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rsync -avr --delete-after ./__site/ /home/antineutrino/blog__site 
+rsync -avr --exclude=".git/" --delete-after ./__site/ /home/antineutrino/blog__site 
 cd /home/antineutrino/blog__site
 git add  -fA 
 git commit -am "commit `date`"
