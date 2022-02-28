fef9f09bff05b7753f94c0e2705d7e76335a99f3
John George Francis <arbitrandomuser@gmail.com>
2022-03-01 01:36:45 +0530

    commit Tue Mar  1 01:36:45 AM IST 2022

--- assets/javisstuff/getting_started/code/gettingstarted.jl
+++ assets/javisstuff/getting_started/code/gettingstarted.jl
@@ -3,6 +3,7 @@ using Luxor
 Drawing()
 move(Point(0,0))
 line( Point(0,0) , Point(100,0) )
-line( currentpoint(), Point(100,100) )
+line(currentpoint(), Point(100,100) )
+#check
 finish()
 preview()
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-rsync -avr --exclude=".git/" --delete-after ./__site/ /home/antineutrino/blog__site 
+#!/usr/bin/bash
+set -e
+julia -e 'using Franklin;Franklin.optimize(;minify=false)'
+rsync -avr --exclude=".git/"  --delete-after ./__site/ /home/antineutrino/blog__site 
 cd /home/antineutrino/blog__site
 git add  -fA 
 git commit -am "commit `date`"
