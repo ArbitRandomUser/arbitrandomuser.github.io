eb3c082eb9f82a9c985961ebf98f4768b17270f1
John George Francis <arbitrandomuser@gmail.com>
2022-06-24 22:19:01 +0530

    commit Fri Jun 24 10:19:01 PM IST 2022

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 set -e
-julia -e 'using Franklin;Franklin.optimize(;minify=false)'
+julia --project=/home/antineutrino/blog -e 'using Franklin;Franklin.optimize(;minify=false)'
 rsync -avr --exclude=".git/"  --delete-after ./__site/ /home/antineutrino/blog/blog__site 
 cd /home/antineutrino/blog/blog__site
 git add  -fA 
